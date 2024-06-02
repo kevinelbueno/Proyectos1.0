098fb293c215c4291cdab4180bcf2031d3a72073
kevinelbueno <kevi.alvarado@duocuc.cl>
2024-06-02 16:15:50 -0400

    version 2.2

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+// /js/loadMenu.js
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('menu-container').innerHTML = data;
+        });
+});
